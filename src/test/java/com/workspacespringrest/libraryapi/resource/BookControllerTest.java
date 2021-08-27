36297f17854a36353a3ab6334510bed2b92f4b95
Brendon Machado <benmacario.s@gmail.com>
2021-08-27 19:18:25 -0300

    first commit

---
+++ src/test/java/com/workspacespringrest/libraryapi/resource/BookControllerTest.java
@@ -0,0 +1,43 @@
+package com.workspacespringrest.libraryapi.resource;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
+@WebMvcTest
+@AutoConfigureMockMvc
+public class BookControllerTest {
+  static String BOOK_API = "v1/api/books";
+
+  @Autowired
+  MockMvc mvc;
+
+  @Test
+  @DisplayName("Successful book creation.")
+  public void createBookTest() throws Exception {
+    String json = new ObjectMapper().writeValueAsString(null);
+
+    MockHttpServletRequestBuilder request = MockMvcRequestBuilders.post(BOOK_API)
+        .contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON).content(json);
+
+    mvc.perform(request).andExpect(MockMvcResultMatchers.status().isCreated())
+        .andExpect(MockMvcResultMatchers.jsonPath("id").isNotEmpty())
+        .andExpect(MockMvcResultMatchers.jsonPath("title").value("Meu Livro"))
+        .andExpect(MockMvcResultMatchers.jsonPath("author").value("Autor"))
+        .andExpect(MockMvcResultMatchers.jsonPath("isbn").value("123456"));
+  }
+}
